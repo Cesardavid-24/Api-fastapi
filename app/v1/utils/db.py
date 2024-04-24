802a68a9da41f8784f119836a3770c847efec10c
cesar <norsciacesar@gmail.com>
2024-04-24 10:52:55 -0400

    fix(backend):Change settings pydantic

--- app/v1/utils/db.py
+++ app/v1/utils/db.py
@@ -12,28 +12,30 @@ DB_PASS = settings.db_pass
 DB_HOST = settings.db_host
 DB_PORT = settings.db_port
 
+
 db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
 db_state = ContextVar("db_state", default=db_state_default.copy())
 
-
 class PeeweeConnectionState(peewee._ConnectionState):
-
     def __init__(self, **kwargs):
-        super().__setattr__("__state", db_state)
+        super().__setattr__("_state", db_state)
         super().__init__(**kwargs)
 
     def __setattr__(self, name, value):
         self._state.get()[name] = value
 
-    def __getatter__(self,name):
+    def __getattr__(self, name):
         return self._state.get()[name]
-    
-db = peewee.MySQLDatabase(DB_NAME,DB_USER,DB_PASS, DB_HOST, DB_PORT)
+
+
+db = peewee.MySQLDatabase(DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+
 db._state = PeeweeConnectionState()
 
 async def reset_db_state():
     db._state._state.set(db_state_default.copy())
     db._state.reset()
+
 
 def get_db(db_state=Depends(reset_db_state)):
     try:
