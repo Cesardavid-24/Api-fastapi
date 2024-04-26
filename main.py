a460b1286c0774341ec8e1072cc3d476a38fed6c
cesar <norsciacesar@gmail.com>
2024-04-25 22:39:19 -0400

    fix(backend): Add enpoint to create user in main app

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 
+from app.v1.router.user_router import router as user_router
 # import uvicorn
 app = FastAPI()
-
-@app.get('/')
-def index():
-    return {"message" : "Hello World"}
+app.include_router(user_router)
 
 # opcional inicarlo desde python 
 #if __name__ == '__main__':
