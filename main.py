37c5ee62bc557bf891b6cdab4c554d32a1ee691a
cesar <norsciacesar@gmail.com>
2024-05-05 11:56:59 -0400

    Add root endpoint

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
 from app.v1.router.user_router import router as user_router
-# import uvicorn
+
 app = FastAPI()
 app.include_router(user_router)
 
-# opcional inicarlo desde python 
-#if __name__ == '__main__':
-    #uvicorn.run(app, host="127.0.0.1", port=4321, reload=True)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
